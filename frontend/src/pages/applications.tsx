18c8f77dcde49b374e6d390ecb17e64826933716
danphenderson <danphenderson@gmail.com>
2023-12-04 14:46:40 -0700

    Updated leads and applications pages, wip.

--- frontend/src/pages/applications.tsx
+++ frontend/src/pages/applications.tsx
@@ -1,12 +1,68 @@
-import React from 'react';
-import { Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Paper, Typography } from '@mui/material';
 
-const Applications: React.FC = () => {
-  return (
-    <>
-      <Typography variant='h4'>Applications Page</Typography>
-    </>
-  );
+interface CoverLetterTemplate {
+  id: string;
+  content: string;
 }
 
-export default Applications;
+const ApplicationsPage: React.FC = () => {
+  const [templates, setTemplates] = useState<CoverLetterTemplate[]>([]);
+  const [editingTemplate, setEditingTemplate] = useState<CoverLetterTemplate | null>(null);
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const handleDialogOpen = (template?: CoverLetterTemplate) => {
+    setEditingTemplate(template || { id: '', content: '' });
+    setDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+    setEditingTemplate(null);
+  };
+
+  const handleSave = () => {
+    if (editingTemplate) {
+      // Logic to save or update the template
+      // Add or update the template in the `templates` array
+      handleDialogClose();
+    }
+  };
+
+  return (
+    <Box sx={{ p: 2 }}>
+      <Button variant="contained" onClick={() => handleDialogOpen()}>
+        Create New Template
+      </Button>
+      {templates.map((template) => (
+        <Paper key={template.id} sx={{ p: 2, my: 2 }}>
+          <Typography variant="h6">Template Preview:</Typography>
+          <Typography variant="body1">{template.content}</Typography>
+          <Button onClick={() => handleDialogOpen(template)}>Edit</Button>
+          {/* Add Delete Button Logic */}
+        </Paper>
+      ))}
+
+      <Dialog open={dialogOpen} onClose={handleDialogClose}>
+        <DialogTitle>{editingTemplate?.id ? 'Edit Template' : 'New Template'}</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Cover Letter Content"
+            fullWidth
+            multiline
+            rows={4}
+            margin="dense"
+            value={editingTemplate?.content || ''}
+            onChange={(e) => setEditingTemplate({ ...editingTemplate || {id : '', content: ''}, content: e.target.value })}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose}>Cancel</Button>
+          <Button onClick={handleSave}>Save</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
+};
+
+export default ApplicationsPage;
--- frontend/src/pages/leads.tsx
+++ frontend/src/pages/leads.tsx
@@ -1,12 +1,95 @@
-import React from 'react';
-import { Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Paper } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
-const Leads: React.FC = () => {
-  return (
-    <>
-      <Typography variant='h4'>Leads Page</Typography>
-    </>
-  );
-}
+interface JobLead {
+    id: string;
+    created_at: string;
+    updated_at: string;
+    title: string;
+    company: string;
+    description: string;
+    location: string;
+    salary: string;
+    job_function: string;
+    industries: string;
+    employment_type: string;
+    seniority_level: string;
+    url: string;
+  }
 
-export default Leads;
+  interface JobSearchRequest {
+    keywords: string;
+    platform: string;
+    location: string;
+  }
+
+
+const LeadsPage: React.FC = () => {
+    const [jobLeads, setJobLeads] = useState<JobLead[]>([]);
+    const [openDialog, setOpenDialog] = useState(false);
+    const [jobSearch, setJobSearch] = useState<JobSearchRequest>({ keywords: '', platform: '', location: '' });
+
+    const handleOpenDialog = () => setOpenDialog(true);
+    const handleCloseDialog = () => setOpenDialog(false);
+
+    const executeSearch = async () => {
+        // Logic to execute search and trigger ETL pipeline
+        handleCloseDialog();
+        // Fetch and set job leads here
+    };
+
+    const columns: GridColDef[] = [
+        { field: 'title', headerName: 'Title', width: 200 },
+        { field: 'company', headerName: 'Company', width: 150 },
+        // ... other columns based on JobLead properties
+    ];
+
+    return (
+        <Box sx={{ p: 2 }}>
+            <Button variant="contained" onClick={handleOpenDialog}>
+                Create Job Search
+            </Button>
+
+            <Dialog open={openDialog} onClose={handleCloseDialog}>
+                <DialogTitle>Create a Job Search</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        label="Keywords"
+                        fullWidth
+                        margin="dense"
+                        value={jobSearch.keywords}
+                        onChange={(e) => setJobSearch({ ...jobSearch, keywords: e.target.value })}
+                    />
+                    <TextField
+                        label="Platform"
+                        fullWidth
+                        margin="dense"
+                        value={jobSearch.platform}
+                        onChange={(e) => setJobSearch({ ...jobSearch, platform: e.target.value })}
+                    />
+                    <TextField
+                        label="Location"
+                        fullWidth
+                        margin="dense"
+                        value={jobSearch.location}
+                        onChange={(e) => setJobSearch({ ...jobSearch, location: e.target.value })}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDialog}>Cancel</Button>
+                    <Button onClick={executeSearch}>Search</Button>
+                </DialogActions>
+            </Dialog>
+
+            <Box sx={{ height: 400, width: '100%', mt: 2 }}>
+                <DataGrid
+                    rows={jobLeads}
+                    columns={columns}
+                />
+            </Box>
+        </Box>
+    );
+};
+
+export default LeadsPage;

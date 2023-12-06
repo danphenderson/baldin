de31d78e5d74fb6963a0ecb367681fc37ca76655
danphenderson <danphenderson@gmail.com>
2023-12-06 15:49:40 -0700

    Successfully implemented user contacts api.

--- backend/app/api/routes/contacts.py
+++ backend/app/api/routes/contacts.py
@@ -1,10 +1,9 @@
 # app/api/contacts.py
 from fastapi import APIRouter, Depends, HTTPException
-from pydantic import UUID4
 from sqlalchemy import select
 
 from app import models, schemas
-from app.api.deps import get_async_session, get_contact, get_current_user
+from app.api.deps import get_async_session, get_contact
 
 router: APIRouter = APIRouter()
 
@@ -35,8 +34,45 @@ async def create_user_contact(
     return contact
 
 
+@router.get("/", response_model=list[schemas.ContactRead])
+async def get_all_contacts(
+    db=Depends(get_async_session),
+):
+    rows = await db.execute(select(models.Contact))
+    result = rows.scalars().all()
+    if not result:
+        raise HTTPException(status_code=404, detail="No contacts found")
+    return result
+
+
 @router.get("/{contact_id}", response_model=schemas.ContactRead)
-async def get_user_contact(
+async def get_contact_by_id(
     contact: models.Contact = Depends(get_contact),
 ):
     return contact
+
+
+@router.patch("/{contact_id}", response_model=schemas.ContactRead)
+async def update_contact_by_id(
+    payload: schemas.ContactUpdate,
+    contact: models.Contact = Depends(get_contact),
+    db=Depends(get_async_session),
+):
+    # Update the contact's attributes
+    for var, value in payload.dict(exclude_unset=True).items():
+        setattr(contact, var, value)
+
+    await db.commit()
+    await db.refresh(contact)
+
+    return contact
+
+
+@router.delete("/{contact_id}", status_code=204)
+async def delete_contact_by_id(
+    contact: models.Contact = Depends(get_contact),
+    db=Depends(get_async_session),
+):
+    db.delete(contact)
+    await db.commit()
+    return None

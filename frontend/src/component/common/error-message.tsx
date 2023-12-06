4252bd7971d90f1c84d6e64488c73a138e93ff70
danphenderson <danphenderson@gmail.com>
2023-12-05 18:51:08 -0700

    improved functionality and styling of error-message.tsx

--- frontend/src/component/common/error-message.tsx
+++ frontend/src/component/common/error-message.tsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { Alert } from '@mui/material';
 
 // Define a type for the component's props
 interface ErrorMessageProps {
   message: string;
+  onClose?: () => void;  // Optional close handler
 }
 
 // Use the ErrorMessageProps type for the component's props
-const ErrorMessage: React.FC<ErrorMessageProps> = ({ message }) => (
-  <p className="has-text-weight-bold has-text-danger">{message}</p>
-);
+const ErrorMessage: React.FC<ErrorMessageProps> = ({ message, onClose }) => {
+  if (!message) {
+    return null; // Do not render if there's no message
+  }
+
+  return (
+    <Alert
+      severity="error"
+      onClose={onClose} // Show close button if onClose is provided
+      sx={{ width: '100%', marginTop: 2, marginBottom: 2 }} // Some spacing and full width
+    >
+      {message}
+    </Alert>
+  );
+};
 
 export default ErrorMessage;

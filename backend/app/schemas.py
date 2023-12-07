c16a70fbead7228c1e0ae75b0e8e361a0db23ee4
danphenderson <danphenderson@gmail.com>
2023-12-06 22:35:40 -0700

    Normalized and improved models

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,5 +1,3 @@
-# app/schemas.py
-
 from datetime import datetime
 
 from fastapi_users import schemas
@@ -9,7 +7,7 @@ from pydantic import BaseModel as _BaseModel
 
 class BaseModel(_BaseModel):
     class Config:
-        from_attributes = True
+        orm_mode = True
 
 
 class BaseRead(BaseModel):
@@ -18,31 +16,61 @@ class BaseRead(BaseModel):
     updated_at: datetime
 
 
-# Begin Schema definitions for model CRUD
+# Model CRUD Schemas
+class BaseETLEvent(BaseModel):
+    job_name: str | None = None
+    status: str | None = None
+    start_time: datetime | None = None
+    end_time: datetime | None = None
+    error_message: str | None = None
 
 
-class BaseUser(BaseModel):
-    first_name: str | None = None
-    last_name: str | None = None
-    phone_number: str | None = None
-    address_line_1: str | None = None
-    address_line_2: str | None = None
-    city: str | None = None
-    state: str | None = None
-    zip_code: str | None = None
-    country: str | None = None
-    skills: str | None = None  # FIXME: going to be a list of strings
-
-
-class UserRead(schemas.BaseUser[UUID4], BaseUser):  # type: ignore
+class ETLEventRead(BaseRead, BaseETLEvent):
     pass
 
 
-class UserCreate(schemas.BaseUserCreate, BaseUser):
+class ETLEventCreate(BaseETLEvent):
     pass
 
 
-class UserUpdate(schemas.BaseUserUpdate, BaseUser):
+class ETLEventUpdate(BaseETLEvent):
+    pass
+
+
+class BaseSkill(BaseModel):
+    name: str | None = None
+    category: str | None = None
+
+
+class SkillRead(BaseRead, BaseSkill):
+    user_id: UUID4
+
+
+class SkillCreate(BaseSkill):
+    user_id: UUID4
+
+
+class SkillUpdate(BaseSkill):
+    pass
+
+
+class BaseExperience(BaseModel):
+    title: str | None = None
+    company: str | None = None
+    start_date: datetime | None = None
+    end_date: datetime | None = None
+    description: str | None = None
+
+
+class ExperienceRead(BaseExperience, BaseRead):
+    user_id: UUID4
+
+
+class ExperienceCreate(BaseExperience):
+    user_id: UUID4
+
+
+class ExperienceUpdate(BaseExperience):
     pass
 
 
@@ -53,13 +81,14 @@ class BaseLead(BaseModel):
     location: str | None = None
     salary: str | None = None
     job_function: str | None = None
-    industries: str | None = None
+    industry: str | None = None
     employment_type: str | None = None
-    seniority_level: str | None = None
+    experience_level: str | None = None
+    education_level: str | None = None
     notes: str | None = None
 
 
-class LeadRead(BaseLead, BaseRead):
+class LeadRead(BaseRead, BaseLead):
     url: str
 
 
@@ -78,33 +107,18 @@ class BaseApplication(BaseModel):
     status: str | None = None
 
 
-class ApplicationRead(BaseApplication, BaseRead):
-    lead: LeadRead | None = None
+class ApplicationRead(BaseRead, BaseApplication):
+    user_id: UUID4
+    lead_id: UUID4
 
 
 class ApplicationCreate(BaseApplication):
+    user_id: UUID4
     lead_id: UUID4
 
 
 class ApplicationUpdate(BaseApplication):
     pass
-
-
-class BaseETLEvent(BaseModel):
-    job_name: str | None = None
-    status: str | None = None
-
-
-class ETLEventRead(BaseETLEvent, BaseRead):
-    pass
-
-
-class ETLEventCreate(BaseETLEvent):
-    pass
-
-
-class ETLEventUpdate(BaseETLEvent):
-    id: UUID4
 
 
 class BaseContact(BaseModel):
@@ -116,8 +130,8 @@ class BaseContact(BaseModel):
     notes: str | None = None
 
 
-class ContactRead(BaseContact, BaseRead):
-    pass
+class ContactRead(BaseRead, BaseContact):
+    user_id: UUID4
 
 
 class ContactCreate(BaseContact):
@@ -125,4 +139,65 @@ class ContactCreate(BaseContact):
 
 
 class ContactUpdate(BaseContact):
+    pass
+
+
+class BaseResume(BaseModel):
+    name: str | None = None
+    content: str | None = None
+    content_type: str | None = None
+
+
+class ResumeRead(BaseRead, BaseResume):
+    user_id: UUID4
+
+
+class ResumeCreate(BaseResume):
+    user_id: UUID4
+
+
+class ResumeUpdate(BaseResume):
+    pass
+
+
+class BaseCoverLetter(BaseModel):
+    name: str | None = None
+    content: str | None = None
+    content_type: str | None = None
+
+
+class CoverLetterRead(BaseRead, BaseCoverLetter):
+    user_id: UUID4
+
+
+class CoverLetterCreate(BaseCoverLetter):
+    user_id: UUID4
+
+
+class CoverLetterUpdate(BaseCoverLetter):
+    pass
+
+
+class BaseUser(schemas.BaseUser):
+    first_name: str | None = None
+    last_name: str | None = None
+    phone_number: str | None = None
+    address_line_1: str | None = None
+    address_line_2: str | None = None
+    city: str | None = None
+    state: str | None = None
+    zip_code: str | None = None
+    country: str | None = None
+    time_zone: str | None = None
+
+
+class UserRead(BaseUser, BaseRead):
+    pass
+
+
+class UserCreate(BaseUser):
+    pass
+
+
+class UserUpdate(BaseUser):
     pass

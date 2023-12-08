ba9971a99153f36120a8b7eb2d6db90a7056199e
danphenderson <danphenderson@gmail.com>
2023-12-08 01:13:16 -0700

    Displaying a paginated leads view.

--- frontend/src/pages/leads.tsx
+++ frontend/src/pages/leads.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { DataGrid, GridColDef, GridPagination } from '@mui/x-data-grid';
+import { DataGrid, GridColDef  } from '@mui/x-data-grid';
 import { Button, Box, Snackbar, Alert } from '@mui/material';
-import LeadModal from '../component/lead-modal';
+import CreateLeadModal from '../component/lead-modal';
 import { components } from '../schema.d';
 
 type LeadRead = components['schemas']['LeadRead'];
@@ -10,36 +10,42 @@ type LeadRead = components['schemas']['LeadRead'];
 const Leads: React.FC = () => {
   const [leads, setLeads] = useState<LeadRead[]>([]);
   const [loading, setLoading] = useState(false);
+  const [totalLeads, setTotalLeads] = useState(0);
   const [selectedLead, setSelectedLead] = useState<LeadRead | null>(null);
   const [modalOpen, setModalOpen] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const [page, setPage] = useState(0);
-  const [pageSize, setPageSize] = useState(5);
+  const [paginationModel, setPaginationModel] = React.useState({
+    pageSize: 5,
+    page: 1,
+  });
 
 
-  // Fetch leads from your API
+
   useEffect(() => {
     const fetchLeads = async () => {
       setLoading(true);
       try {
-        // Replace with your API call
-        const response = await fetch(`/leads/?page=${page}&size=${pageSize}`);
+
+        const response = await fetch(`/leads/?page=${paginationModel.page}&page_size=${paginationModel.pageSize}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
         const data = await response.json();
-        setLeads(data);
+        setLeads(data.leads); // Assuming the response has a 'leads' property
+        setTotalLeads(data.total_count); // Assuming the response has a 'totalCount' property
       } catch (error) {
         console.error('Failed to fetch leads:', error);
-        setError('Failed to load leads');
+        setError(typeof error === 'string' ? error : 'Failed to load leads');
       }
       setLoading(false);
     };
-
     fetchLeads();
-  }, [page, pageSize]);
+  }, [paginationModel.page, paginationModel.pageSize]);
 
-  // const handleAddLead = () => {
-  //   setSelectedLead(null);
-  //   setModalOpen(true);
-  // };
+
+  const handlePageSizeChange = (pageSize: number) => {
+    setPaginationModel({ ...paginationModel, pageSize: pageSize, page: 1 });
+  };
 
   const handleEditLead = (id: string) => {
     const lead = leads.find(l => l.id === id);
@@ -59,10 +65,7 @@ const Leads: React.FC = () => {
   };
 
   const handleSaveLead = (lead: Omit<LeadRead, 'id' | 'created_at' | 'updated_at'>) => {
-    // Save lead logic
-    // Update state with new or edited lead
     setModalOpen(false);
-    // Refresh the list or update the state with new data
   };
 
   const handleCloseSnackbar = () => {
@@ -76,7 +79,7 @@ const Leads: React.FC = () => {
     { field: 'description', headerName: 'Description', width: 200 },
     // { field: 'location', headerName: 'Location', width: 150 },
     // { field: 'salary', headerName: 'Salary', width: 130 },
-    { field: 'job_function', headerName: 'Job Function', width: 150 },
+    // { field: 'job_function', headerName: 'Job Function', width: 150 },
     { field: 'industries', headerName: 'Industries', width: 150 },
     { field: 'employment_type', headerName: 'Employment Type', width: 150 },
     { field: 'seniority_level', headerName: 'Seniority Level', width: 150 },
@@ -91,20 +94,11 @@ const Leads: React.FC = () => {
         <Button onClick={() => handleEditLead(params.row.id)}>Edit</Button>
       ),
     },
-    {
-      field: 'actions',
-      headerName: 'Actions',
-      sortable: false,
-      width: 150,
-      renderCell: (params) => (
-        <Button onClick={() => handleEditLead(params.row.id)}>Edit</Button>
-      ),
-    },
   ];
 
   return (
+    <div>
     <Box sx={{ p: 2 }}>
-      {/* UI Elements here */}
       <Button variant="contained" onClick={handleAddLead} sx={{ mb: 2 }}>
         Add New Lead
       </Button>
@@ -115,13 +109,20 @@ const Leads: React.FC = () => {
         disableRowSelectionOnClick
         onRowDoubleClick={(params) => handleEditLead(params.id.toString())}
         pagination
-        rowCount={100} // Replace with total row count from server
-        components={{
-          Pagination: GridPagination,
+        pageSizeOptions={[5, 10]}
+        rowCount={totalLeads}
+        paginationMode="server"
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: paginationModel.pageSize,
+              page: paginationModel.page  // default value will be used if not passed */
+            },
+          },
         }}
+        onPaginationModelChange={setPaginationModel}
       />
-
-      <LeadModal
+      <CreateLeadModal
         open={modalOpen}
         onClose={() => setModalOpen(false)}
         onSave={handleSaveLead}
@@ -148,6 +149,7 @@ const Leads: React.FC = () => {
         </Snackbar>
       )}
     </Box>
+    </div>
   );
 };
 

e26612f5baf683a81ae0eaaaf261287a2e6a3030
danphenderson <danphenderson@gmail.com>
2023-12-07 21:16:27 -0700

    Documentation updates, and built out base fixtures for testing.

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -1,5 +1,4 @@
-import asyncio
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Callable
 
 import pytest
 from fastapi_users.password import PasswordHelper
@@ -9,8 +8,19 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core import conf
 from app.core.db import async_engine
 from app.core.db import async_session_maker as async_session
+from app.core.db import drop_and_create_db_and_tables
 from app.main import app
-from app.models import Base
+from app.models import (
+    Application,
+    Contact,
+    CoverLetter,
+    ETLEvent,
+    Experience,
+    Lead,
+    Resume,
+    Skill,
+    User,
+)
 from app.tests import utils
 
 password_helper = PasswordHelper()
@@ -22,38 +32,80 @@ superuser_user_hash = password_helper.hash("yennefer")
 
 
 @pytest.fixture(scope="session")
-async def test_client():
+async def test_client() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(app=app, base_url="http://doesnt.matter") as client:
         yield client
 
 
 @pytest.fixture(scope="session")
-async def test_db_setup_sessionmaker():
-    # assert if we use TEST_DB URL for 100%
+async def test_db_setup_sessionmaker() -> Callable[
+    ..., AsyncGenerator[AsyncSession, None]
+]:
     assert conf.settings.ENVIRONMENT == "PYTEST"
     assert str(async_engine.url) == conf.settings.TEST_SQLALCHEMY_DATABASE_URI
 
-    # always drop and create test db tables between tests session
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+    await drop_and_create_db_and_tables()
 
     return async_session
 
 
 @pytest.fixture
-async def session(test_db_setup_sessionmaker) -> AsyncGenerator[AsyncSession, None]:
+async def session(
+    test_db_setup_sessionmaker: Callable[..., AsyncGenerator[AsyncSession, None]]
+) -> AsyncGenerator[AsyncSession, None]:
     async with test_db_setup_sessionmaker() as session:
         yield session
 
 
 @pytest.fixture
-async def default_user(session: AsyncSession):
+async def default_user(session: AsyncSession) -> User:
     return await utils.create_db_user(default_user_email, default_user_hash, session)
 
 
 @pytest.fixture
-async def superuser_user(session: AsyncSession):
+async def superuser_user(session: AsyncSession) -> User:
     return await utils.create_db_user(
         superuser_user_email, superuser_user_hash, session, is_superuser=True
     )
+
+
+@pytest.fixture
+async def etl_event(session: AsyncSession) -> ETLEvent:
+    return await utils.create_etl_event(session)
+
+
+@pytest.fixture
+async def lead(session: AsyncSession) -> Lead:
+    return await utils.create_lead(session)
+
+
+@pytest.fixture
+async def skill(session: AsyncSession, default_user: User) -> Skill:
+    return await utils.create_skill(session, default_user.id)
+
+
+@pytest.fixture
+async def experience(session: AsyncSession, default_user: User) -> Experience:
+    return await utils.create_experience(session, default_user.id)
+
+
+@pytest.fixture
+async def application(
+    session: AsyncSession, default_user: User, lead: Lead
+) -> Application:
+    return await utils.create_application(session, default_user.id, lead.id)
+
+
+@pytest.fixture
+async def contact(session: AsyncSession, default_user: User) -> Contact:
+    return await utils.create_contact(session, default_user.id)
+
+
+@pytest.fixture
+async def resume(session: AsyncSession, default_user: User) -> Resume:
+    return await utils.create_resume(session, default_user.id)
+
+
+@pytest.fixture
+async def cover_letter(session: AsyncSession, default_user: User) -> CoverLetter:
+    return await utils.create_cover_letter(session, default_user.id)
--- backend/app/tests/utils.py
+++ backend/app/tests/utils.py
@@ -1,12 +1,22 @@
 import random
 import string
+from datetime import datetime, timedelta
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
-from pydantic.networks import EmailStr
+from pydantic import UUID4
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models import User
-from app.schemas import UserRead
+from app.models import (
+    Application,
+    Contact,
+    CoverLetter,
+    ETLEvent,
+    Experience,
+    Lead,
+    Resume,
+    Skill,
+    User,
+)
 
 
 def random_lower_string(length: int = 32) -> str:
@@ -23,8 +33,7 @@ async def create_db_user(
     session: AsyncSession,
     is_superuser: bool = False,
     is_verified: bool = True,
-) -> UserRead:
-
+) -> User:
     new_user = await SQLAlchemyUserDatabase(session, User).create(
         {
             "email": email,
@@ -33,4 +42,107 @@ async def create_db_user(
             "is_verified": is_verified,
         }
     )
-    return UserRead(**new_user.__dict__)
+    return new_user
+
+
+async def create_etl_event(session: AsyncSession):
+    etl_event = ETLEvent(
+        job_name="Test Job",
+        status=random.choice(["pending", "running", "success", "failed"]),
+        error_message="Test error" if random.choice([True, False]) else None,
+    )
+    session.add(etl_event)
+    await session.commit()
+    return etl_event
+
+
+async def create_lead(session: AsyncSession):
+    lead = Lead(
+        url="http://example.com/job",
+        title="Test Job Title",
+        company="Test Company",
+        description="Test Job Description",
+        location="Test Location",
+        salary="50,000 - 70,000",
+        job_function="Test Function",
+        industries="Test Industry",
+        employment_type="Full-Time",
+        seniority_level="Mid-Level",
+        education_level="Bachelor's",
+        notes="Test Note",
+    )
+    session.add(lead)
+    await session.commit()
+    return lead
+
+
+async def create_skill(session: AsyncSession, user_id: UUID4):
+    skill = Skill(name="Test Skill", category="Test Category", user_id=user_id)
+    session.add(skill)
+    await session.commit()
+    return skill
+
+
+async def create_experience(session: AsyncSession, user_id: UUID4):
+    experience = Experience(
+        title="Test Job Title",
+        company="Test Company",
+        start_date=datetime.now() - timedelta(days=365),
+        end_date=datetime.now(),
+        description="Test Experience Description",
+        user_id=user_id,
+    )
+    session.add(experience)
+    await session.commit()
+    return experience
+
+
+async def create_application(session: AsyncSession, user_id: UUID4, lead_id: UUID4):
+    application = Application(
+        cover_letter="Test Cover Letter",
+        status="submitted",
+        lead_id=lead_id,
+        user_id=user_id,
+    )
+    session.add(application)
+    await session.commit()
+    return application
+
+
+async def create_contact(session: AsyncSession, user_id: UUID4):
+    contact = Contact(
+        first_name="Test",
+        last_name="Contact",
+        phone_number="123-456-7890",
+        email="test.contact@example.com",
+        time_zone="UTC",
+        notes="Test Note",
+        user_id=user_id,
+    )
+    session.add(contact)
+    await session.commit()
+    return contact
+
+
+async def create_resume(session: AsyncSession, user_id: UUID4):
+    resume = Resume(
+        name="Test Resume",
+        content="Test Content",
+        content_type="custom",
+        user_id=user_id,
+    )
+    session.add(resume)
+    await session.commit()
+    return resume
+
+
+async def create_cover_letter(session: AsyncSession, user_id: UUID4):
+    cover_letter = CoverLetter(
+        name="Test Cover Letter",
+        content="Test Content",
+        content_type="custom",
+        user_id=user_id,
+    )
+    session.add(cover_letter)
+    await session.commit()
+    return cover_letter

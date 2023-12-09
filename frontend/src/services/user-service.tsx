6c5256639a6f8b4ed9d918372db389e97cea4809
danphenderson <danphenderson@gmail.com>
2023-12-09 15:06:02 -0700

    User-settings are now using user-service.tsx to hit api.

--- frontend/src/services/user-service.tsx
+++ frontend/src/services/user-service.tsx
@@ -1,6 +1,7 @@
 import {components} from '../schema.d';
 
-type UserRead = components['schemas']['UserRead'];
+export type UserRead = components['schemas']['UserRead'];
+export type UserUpdate = components['schemas']['UserUpdate'];
 
 export const getUser = async (token: string): Promise<UserRead | null> => {
   if (!token) {
@@ -21,3 +22,24 @@ export const getUser = async (token: string): Promise<UserRead | null> => {
   const userData: UserRead = await response.json();
   return userData;
 };
+
+export const updateUser = async (token: string, user: UserUpdate): Promise<UserRead | null> => {
+  if (!token) {
+    return null;
+  }
+  const requestOptions = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + token,
+    },
+    body: JSON.stringify(user),
+  };
+  const response = await fetch("/users/me", requestOptions);
+
+  if (!response.ok) {
+    return null;
+  }
+  const userData: UserRead = await response.json();
+  return userData;
+}

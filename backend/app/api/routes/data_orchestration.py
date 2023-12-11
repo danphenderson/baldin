d8764198fd0c16c45967a255eaaad4d4036df48c
danphenderson <danphenderson@gmail.com>
2023-12-10 21:24:12 -0700

    data_orchestration appears to be working.

--- backend/app/api/routes/data_orchestration.py
+++ backend/app/api/routes/data_orchestration.py
@@ -1,12 +1,12 @@
 # app/api/routes/etl.py
 import json
+
 from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
 from pydantic import UUID4
 from sqlalchemy import select
 
-from app.api.deps import (
+from app.api.deps import (  # noqa
     AsyncSession,
-    conf,
     database_load,
     execute_leads_enrichment,
     execute_load_leads,
@@ -15,10 +15,10 @@ from app.api.deps import (
     models,
     schemas,
 )
-
 from app.logging import console_log
 
 router: APIRouter = APIRouter()
+
 
 @router.post("/run", status_code=202, response_model=schemas.OrchestrationEventRead)
 async def create_orch_event(
@@ -28,8 +28,8 @@ async def create_orch_event(
 ):
     # Serialization when creating a new event
     event_dict = orch_event.dict()
-    event_dict['source_uri'] = json.dumps(event_dict['source_uri'])
-    event_dict['destination_uri'] = json.dumps(event_dict['destination_uri'])
+    event_dict["source_uri"] = json.dumps(event_dict["source_uri"])
+    event_dict["destination_uri"] = json.dumps(event_dict["destination_uri"])
     event = models.OrchestrationEvent(**event_dict)
 
     # Create orchestration event
@@ -50,20 +50,12 @@ async def create_orch_event(
     # # Run orchestration pipeline in the background
     background_tasks.add_task(pipeline, event.id)
 
-    event.source_uri = json.loads(getattr(event, "source_uri"))  # Deserialize into URI object
+    event.source_uri = json.loads(
+        getattr(event, "source_uri")
+    )  # Deserialize into URI object
     event.destination_uri = json.loads(getattr(event, "destination_uri"))
     return event
 
-@router.get(
-    "/events/{id}", status_code=202, response_model=schemas.OrchestrationEventRead
-)
-async def read_orch_event(
-    id: UUID4, db: AsyncSession = Depends(get_async_session)
-):
-    event = await get_orchestration_event(id, db)
-    event.source_uri = json.loads(getattr(event, "source_uri"))  # Deserialize into URI object
-    event.destination_uri = json.loads(getattr(event, "destination_uri"))  # Deserialize into URI object
-    return event
 
 @router.get("/events", response_model=list[schemas.OrchestrationEventRead])
 async def read_orch_events(db: AsyncSession = Depends(get_async_session)):
@@ -87,12 +79,12 @@ async def read_successful_orch_events(db: AsyncSession = Depends(get_async_sessi
         )
     )
     result = rows.scalars().all()
-    if not result:
-        raise HTTPException(status_code=404, detail="No successful ETL events found")
+    for row in result:
+        row.source_uri = json.loads(getattr(row, "source_uri"))
+        row.destination_uri = json.loads(getattr(row, "destination_uri"))
 
-    for event in result:
-        event.source_uri = json.loads(getattr(event, "source_uri"))
-        event.destination_uri = json.loads(getattr(event, "destination_uri"))
+    if not result:
+        raise HTTPException(status_code=404, detail="No ETL events found")
     return result
 
 
@@ -107,3 +99,17 @@ async def read_failed_orch_events(db: AsyncSession = Depends(get_async_session))
     if not result:
         raise HTTPException(status_code=404, detail="No failed ETL events found")
     return result
+
+
+@router.get(
+    "/events/{id}", status_code=202, response_model=schemas.OrchestrationEventRead
+)
+async def read_orch_event(id: UUID4, db: AsyncSession = Depends(get_async_session)):
+    event = await get_orchestration_event(id, db)
+    event.source_uri = json.loads(
+        getattr(event, "source_uri")
+    )  # Deserialize into URI object
+    event.destination_uri = json.loads(
+        getattr(event, "destination_uri")
+    )  # Deserialize into URI object
+    return event

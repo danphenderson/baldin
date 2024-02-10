4308e45babcc6e94263a4c45a7984ad40f8e38e2
danphenderson <danphenderson@gmail.com>
2024-02-10 14:28:49 -0800

    Removing unused imports.

--- frontend/src/layout/home-layout.tsx
+++ frontend/src/layout/home-layout.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Paper from '@mui/material/Paper';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
 import Header from '../component/common/header';
 import Footer from '../component/common/footer';
-import { Box, Stack } from '@mui/material';
+import { Stack } from '@mui/material';
 
 const HomeLayout: React.FC = () => {
   return (

e4d9c847b53e5b852c907e4c54911f53d2347577
danphenderson <me@danhenderson.dev>
2024-04-07 20:59:50 -0700

    wip: models.py mv'd

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,17 +2,8 @@
 from uuid import uuid4
 
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
-from sqlalchemy import (
-    JSON,
-    UUID,
-    Column,
-    DateTime,
-    ForeignKey,
-    Integer,
-    String,
-    Text,
-    func,
-)
+from sqlalchemy import JSON, Column, DateTime, ForeignKey, Integer, String, Text, func
+from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.orm import DeclarativeBase, relationship
 
 
@@ -23,12 +14,14 @@ class Base(DeclarativeBase):
     Inherits from SQLAlchemy's DeclarativeBase.
     """
 
+    __abstract__ = True
+
     id = Column(UUID, primary_key=True, default=uuid4)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
 
-# System models
+# Data Orchestration models
 class OrchestrationEvent(Base):
     """
     Model for ETL (Extract, Transform, Load) events.
@@ -42,6 +35,51 @@ class OrchestrationEvent(Base):
     destination_uri = Column(JSON)
     status = Column(String)  # running, success, failure
     error_message = Column(Text)
+
+
+class ExtractorExample(Base):
+    """A representation of an example.
+
+    Examples consist of content together with the expected output.
+
+    The output is a JSON object that is expected to be extracted from the content.
+
+    The JSON object should be valid according to the schema of the associated extractor.
+
+    The JSON object is defined by the schema of the associated extractor, so
+    it's perfectly fine for a given example to represent the extraction
+    of multiple instances of some object from the content since
+    the JSON schema can represent a list of objects.
+    """
+
+    __tablename__ = "examples"
+    content = Column(Text, nullable=False, comment="The input portion of the example.")
+    output = Column(JSONB, comment="The output associated with the example.")
+    extractor_id = Column(UUID, ForeignKey("extractors.id"))
+    extractor = relationship("ExtractorExample", back_populates="examples")
+
+    def __repr__(self) -> str:
+        return f"<ExtractorExample(uuid={self.id}, content={self.content[:20]}>"
+
+
+class Extractor(Base):
+    """
+    Represents an extractor for parsing structured data from unstructured text.
+    Contains name, description, schema, instruction, etc.
+    Linked to users and examples via many-to-one and one-to-many relationships.
+    """
+
+    __tablename__ = "extractors"
+    name = Column(String)
+    description = Column(Text)
+    schema = Column(JSONB)
+    instruction = Column(Text)
+    user_id = Column(UUID, ForeignKey("users.id"))
+    user = relationship("User", back_populates="extractors")
+    extractor_examples = relationship("ExtractorExample", back_populates="extractor")
+
+    def __repr__(self) -> str:
+        return f"<Extractor(id={self.id}, description={self.description})>"
 
 
 class Lead(Base):
@@ -256,3 +294,5 @@ class User(SQLAlchemyBaseUserTableUUID, Base):  # type: ignore
     education = relationship("Education", back_populates="user")
     certificates = relationship("Certificate", back_populates="user")
     cover_letters = relationship("CoverLetter", back_populates="user")
+    extractors = relationship("Extractor", back_populates="user")
+    extractor_examples = relationship("ExtractorExample", back_populates="user")

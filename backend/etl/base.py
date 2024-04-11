4b1a58d181f4fd843640e147c762bf31b922b3f6
danphenderson <me@danhenderson.dev>
2024-04-10 17:15:22 -0700

    chore: ensuring old leads selenium etl works

--- backend/etl/base.py
+++ backend/etl/base.py
@@ -8,6 +8,63 @@ from .logging import get_logger
 
 logger = get_logger(__name__)
 
+from asyncio import Future, ensure_future, get_event_loop, sleep
+from pathlib import Path
+
+from aiofiles import open as aopen
+
+from app import schemas
+
+
+class AsyncBaseModel(schemas.BaseSchema, extra="allow"):
+    _tasks: list = []
+
+    @staticmethod
+    async def _run_sync(func, *args, **kwargs):
+        loop = get_event_loop()
+        return await loop.run_in_executor(None, func, *args, **kwargs)
+
+    @classmethod
+    async def load(cls, file_path: str):
+        async with aopen(file_path, "r") as f:
+            data = await f.read()
+            return await cls._run_sync(lambda: cls.parse_raw(data))
+
+    async def run_async(self, func, *args, **kwargs) -> Future:
+        task = ensure_future(self._run_sync(func, *args, **kwargs))
+        self._tasks.append(task)
+        return await task
+
+    async def to_dict(self) -> dict:
+        return await self._run_sync(lambda: self.__dict__)
+
+    async def dump(self, file_path: str, indent: int = 4):
+        Path(file_path).parent.mkdir(parents=True, exist_ok=True)
+        async with aopen(file_path, "a") as f:
+            await f.write(self.json())
+
+    async def wait(self, seconds: int) -> None:
+        if seconds > 0:
+            await sleep(seconds)
+
+    async def __aenter__(self):
+        # Ref: https://peps.python.org/pep-0492/#asynchronous-context-managers-and-async-with
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback) -> None:
+        # Ref: https://peps.python.org/pep-0492/#asynchronous-context-managers-and-async-with
+        pass
+
+    async def __await__(self):
+        return self._run_sync(lambda: self).__await__()
+
+
+class Job(AsyncBaseModel, schemas.LeadCreate):
+    async def dump(self, file_path: str, indent: int = 4):  # Fix: Add the "indent" parameter to match the superclass signature
+        return await super().dump(
+            file_path=file_path,
+            indent=indent,
+        )
 
 class Scrapper:
     def __init__(self):
